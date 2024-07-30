f20f99c8a9c4f8031bbb2147ada07ab105d79d37
VioletaNaboy <121202853+VioletaNaboy@users.noreply.github.com>
2024-07-30 19:30:07 +0300

    get_all_orders

--- handlers/get_all_orders.go
+++ handlers/get_all_orders.go
@@ -1,12 +1,21 @@
 package handlers
 
 import (
+	"log"
+
 	"github.com/gofiber/fiber/v3"
-	"github.com/onsana/order_service/model"
+	"github.com/onsana/order_service/database"
+	"github.com/onsana/order_service/dto"
 )
 
 func GetAllOrders(c fiber.Ctx) error {
-	var orders []model.Order
+	var orders []dto.OrderDto
+
+	result := database.DB.Db.Find(&orders)
+	if result.Error != nil {
+		log.Println(result.Error)
+		return c.Status(fiber.StatusInternalServerError).SendString("Помилка під час отримання замовлень")
+	}
 
 	return c.JSON(orders)
 }
